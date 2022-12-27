787c464bc328349a5b7e48e11c89b03e39850fca
obeespace <94024823+obeespace@users.noreply.github.com>
2022-12-27 16:44:03 +0100

    error check

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counterSlice'
+import counterReducer from './CounterSlice'
 
 export const store = configureStore({
   reducer: {
